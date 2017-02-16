5efafe3f77eb0be9099036b9e08328d94a56fb10
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-02-16 23:45:50 +0100

    fix 1: Finish vector_addition tutorial
    Finished the tutorial, however I need to clean it up a bit.

---
+++ data/projects/vector_addition/build/CMakeFiles/vector_addition.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_addition.dir/vector_addition.c.o"
+  "vector_addition.pdb"
+  "vector_addition"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vector_addition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
