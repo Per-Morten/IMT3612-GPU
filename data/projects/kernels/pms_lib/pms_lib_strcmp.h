595abc987f2dbe4e3f2041d428888d6897d287d4
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-09 11:47:19 +0100

    ref #18: Make generator for pms_lib_strcmp

--- data/projects/kernels/pms_lib/pms_lib_strcmp.h
+++ data/projects/kernels/pms_lib/pms_lib_strcmp.h
@@ -3,225 +3,38 @@
 #include <pms_lib_types.h>
 // Taken from: https://github.com/lattera/glibc/blob/master/string/test-strcmp.c
 
-///////////////////////////////////////////////////////////////////////////////
-/// Global
-///////////////////////////////////////////////////////////////////////////////
-int32_t
-pms_strcmp_gg(__global      const char* lhs,
-              __global      const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__global uchar*)lhs) - 
-                  (*(__global uchar*)rhs++)) == 0 && (*lhs++))
-    {
+#define PMS_LIB_STRCMP_GENERATOR(memory_region_a, memory_region_b, postfix)     \
+int32_t                                                                         \
+pms_strcmp_##postfix(memory_region_a const char* lhs,                           \
+                     memory_region_b const char* rhs)                           \
+{                                                                               \
+    int32_t ret = 0;                                                            \
+    while ((ret = (*(memory_region_a uchar*)lhs) -                              \
+                  (*(memory_region_b uchar*)rhs++)) == 0 && (*lhs++))           \
+    {                                                                           \
+                                                                                \
+    }                                                                           \
+    return ret;                                                                 \
+}                                                                               \
 
-    }
-    return ret;
-}
+PMS_LIB_STRCMP_GENERATOR(__global, __global,   gg);
+PMS_LIB_STRCMP_GENERATOR(__global, __constant, gc);
+PMS_LIB_STRCMP_GENERATOR(__global, __local,    gl);
+PMS_LIB_STRCMP_GENERATOR(__global, __private,  gp);
 
-int32_t
-pms_strcmp_gc(__global      const char* lhs,
-              __constant    const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__global uchar*)lhs) - 
-                  (*(__constant uchar*)rhs++)) == 0 && (*lhs++))
-    {
+PMS_LIB_STRCMP_GENERATOR(__constant, __global,   cg);
+PMS_LIB_STRCMP_GENERATOR(__constant, __constant, cc);
+PMS_LIB_STRCMP_GENERATOR(__constant, __local,    cl);
+PMS_LIB_STRCMP_GENERATOR(__constant, __private,  cp);
 
-    }
-    return ret;
-}
+PMS_LIB_STRCMP_GENERATOR(__local, __global,   lg);
+PMS_LIB_STRCMP_GENERATOR(__local, __constant, lc);
+PMS_LIB_STRCMP_GENERATOR(__local, __local,    ll);
+PMS_LIB_STRCMP_GENERATOR(__local, __private,  lp);
 
-int32_t
-pms_strcmp_gl(__global      const char* lhs,
-              __local       const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__global uchar*)lhs) - 
-                  (*(__local uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_gp(__global      const char* lhs,
-              __private     const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__global uchar*)lhs) - 
-                  (*(__private uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-/// Constant
-///////////////////////////////////////////////////////////////////////////////
-int32_t
-pms_strcmp_cc(__constant    const char* lhs,
-              __constant    const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__constant uchar*)lhs) - 
-                  (*(__constant uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_cg(__constant    const char* lhs,
-              __global      const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__constant uchar*)lhs) - 
-                  (*(__global uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_cl(__constant    const char* lhs,
-              __local       const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__constant uchar*)lhs) - 
-                  (*(__local uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_cp(__constant    const char* lhs,
-              __private     const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__constant uchar*)lhs) - 
-                  (*(__private uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-/// Local
-///////////////////////////////////////////////////////////////////////////////
-int32_t
-pms_strcmp_ll(__local       const char* lhs,
-              __local       const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__local uchar*)lhs) - 
-                  (*(__local uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_lg(__local       const char* lhs,
-              __global      const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__local uchar*)lhs) - 
-                  (*(__global uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_lc(__local       const char* lhs,
-              __constant    const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__local uchar*)lhs) - 
-                  (*(__constant uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_lp(__local       const char* lhs,
-              __private     const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__local uchar*)lhs) - 
-                  (*(__private uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-/// Private
-///////////////////////////////////////////////////////////////////////////////
-
-int32_t
-pms_strcmp_pp(__private     const char* lhs,
-              __private     const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__private uchar*)lhs) - 
-                  (*(__private uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_pg(__private     const char* lhs,
-              __global      const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__private uchar*)lhs) - 
-                  (*(__global uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_pc(__private     const char* lhs,
-              __constant    const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__private uchar*)lhs) - 
-                  (*(__constant uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
-
-int32_t
-pms_strcmp_pl(__private     const char* lhs,
-              __local       const char* rhs)
-{
-    int32_t ret = 0;
-    while ((ret = (*(__private uchar*)lhs) - 
-                  (*(__local uchar*)rhs++)) == 0 && (*lhs++))
-    {
-
-    }
-    return ret;
-}
+PMS_LIB_STRCMP_GENERATOR(__private, __global,   pg);
+PMS_LIB_STRCMP_GENERATOR(__private, __constant, pc);
+PMS_LIB_STRCMP_GENERATOR(__private, __local,    pl);
+PMS_LIB_STRCMP_GENERATOR(__private, __private,  pp);
 
 #endif
