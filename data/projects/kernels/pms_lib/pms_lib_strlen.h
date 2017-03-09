988361e8a85f3846926a58644167de2b12f7a469
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-09 11:48:42 +0100

    ref #18: Make generator for pms_lib_strlen

--- data/projects/kernels/pms_lib/pms_lib_strlen.h
+++ data/projects/kernels/pms_lib/pms_lib_strlen.h
@@ -2,48 +2,21 @@
 #define PMS_LIB_STRLEN
 #include <pms_lib_types.h>
 
-uint32_t
-pms_strlen_g(__global const char* str)
-{
-    uint32_t len = 0;
-    while ((*str++))
-    {
-        len++;
-    }
-    return len;
-}
+#define PMS_LIB_STRLEN_GENERATOR(memory_region_a, postfix)                      \
+size_t                                                                          \
+pms_strlen_##postfix(memory_region_a const char* str)                           \
+{                                                                               \
+    size_t len = 0;                                                             \
+    while ((*str++))                                                            \
+    {                                                                           \
+        len++;                                                                  \
+    }                                                                           \
+    return len;                                                                 \
+}                                                                               \
 
-uint32_t
-pms_strlen_c(__constant const char* str)
-{
-    uint32_t len = 0;
-    while ((*str++))
-    {
-        len++;
-    }
-    return len;
-}
-
-uint32_t
-pms_strlen_l(__local const char* str)
-{
-    uint32_t len = 0;
-    while ((*str++))
-    {
-        len++;
-    }
-    return len;
-}
-
-uint32_t
-pms_strlen_p(__private const char* str)
-{
-    uint32_t len = 0;
-    while ((*str++))
-    {
-        len++;
-    }
-    return len;
-}
+PMS_LIB_STRLEN_GENERATOR(__global,   g);
+PMS_LIB_STRLEN_GENERATOR(__constant, c);
+PMS_LIB_STRLEN_GENERATOR(__local,    l);
+PMS_LIB_STRLEN_GENERATOR(__private,  p);
 
 #endif
