b101cd0f92c79e895a58af52498af49cb030e219
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-23 20:24:41 +0100

    ref #20: Add stack_allocator functionality
    Added a function that emulates stack allocator like behaviour on a
    memory block.

---
+++ data/projects/kernels/pms_lib/pms_lib_allocator.h
@@ -0,0 +1,33 @@
+#ifndef PMS_LIB_ALLOCATOR
+#define PMS_LIB_ALLOCATOR
+
+#define PMS_LIB_STACK_ALLOCATE_GENERATOR(memory_region, postfix)            \
+memory_region void*                                                         \
+pms_stack_allocate_##postfix(memory_region void* memory,                    \
+                             size_t* heap_index,                            \
+                             size_t size)                                   \
+{                                                                           \
+    memory_region void* mem = (memory_region void*)&memory[*heap_index];    \
+    (*heap_index) += size;                                                  \
+    return mem;                                                             \
+}                                                                           \
+
+
+#define PMS_LIB_STACK_DEALLOCATE_GENERATOR(memory_region, postfix)  \
+void                                                                \
+pms_stack_deallocate_##postfix(memory_region void* memory,          \
+                               size_t* heap_index,                  \
+                               size_t size)                         \
+{                                                                   \
+    (*heap_index) -= size;                                          \
+}                                                                   \
+
+PMS_LIB_STACK_ALLOCATE_GENERATOR(__global,   g);
+PMS_LIB_STACK_ALLOCATE_GENERATOR(__local,    l);
+PMS_LIB_STACK_ALLOCATE_GENERATOR(__private,  p);
+
+PMS_LIB_STACK_DEALLOCATE_GENERATOR(__global,   g);
+PMS_LIB_STACK_DEALLOCATE_GENERATOR(__local,    l);
+PMS_LIB_STACK_DEALLOCATE_GENERATOR(__private,  p);
+
+#endif
