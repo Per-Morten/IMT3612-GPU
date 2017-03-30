c21d1d23e024a9b60bb83c635790473d8710581f
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-30 11:31:52 +0200

    ref #4: Move allocator generator
    Moved the stack allocator generator functions to come directly after the
    functions they are generating

--- data/projects/kernels/pms_lib/pms_lib_allocator.h
+++ data/projects/kernels/pms_lib/pms_lib_allocator.h
@@ -12,22 +12,19 @@ pms_stack_allocate_##postfix(memory_region void* memory,                    \
     return mem;                                                             \
 }                                                                           \
 
-
-#define PMS_LIB_STACK_DEALLOCATE_GENERATOR(memory_region, postfix)  \
-void                                                                \
-pms_stack_deallocate_##postfix(memory_region void* memory,          \
-                               size_t* heap_index,                  \
-                               size_t size)                         \
-{                                                                   \
-    (*heap_index) -= size;                                          \
-}                                                                   \
-
 PMS_LIB_STACK_ALLOCATE_GENERATOR(__global,   g);
 PMS_LIB_STACK_ALLOCATE_GENERATOR(__local,    l);
 PMS_LIB_STACK_ALLOCATE_GENERATOR(__private,  p);
 
+#define PMS_LIB_STACK_DEALLOCATE_GENERATOR(memory_region, postfix)          \
+void                                                                        \
+pms_stack_deallocate_##postfix(memory_region void* memory,                  \
+                               size_t* heap_index,                          \
+                               size_t size)                                 \
+{                                                                           \
+    (*heap_index) -= size;                                                  \
+}                                                                           \
+
 PMS_LIB_STACK_DEALLOCATE_GENERATOR(__global,   g);
 PMS_LIB_STACK_DEALLOCATE_GENERATOR(__local,    l);
 PMS_LIB_STACK_DEALLOCATE_GENERATOR(__private,  p);
-
-#endif
